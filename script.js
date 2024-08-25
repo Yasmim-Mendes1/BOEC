30e1d7e60c24f600176ccf6d0dc9a811b78ab45b
Yasmim Mendes <157243378+Yasmim-Mendes1@users.noreply.github.com>
2024-08-25 16:40:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,17 +13,14 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }
 
-  // Inicialmente exibir o conteúdo da página BOEC
   exibirConteudo('boec');
 
   navLinks.forEach(link => {
     link.addEventListener('click', function (e) {
       e.preventDefault();
 
-      // Remove a classe 'active' de todos os links
       navLinks.forEach(navLink => navLink.classList.remove('active'));
 
-      // Adiciona a classe 'active' apenas ao link clicado
       this.classList.add('active');
 
       const page = this.getAttribute('data-page');
